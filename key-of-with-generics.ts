bdc54adc0d3d72b0d17962b37eb8cc45c6a7b15e
mitaly <shubhamsapra@hotmail.com>
2022-10-28 11:13:27 +0530

    keyof Event emitter example

--- key-of-with-generics.ts
+++ key-of-with-generics.ts
@@ -19,3 +19,23 @@ const studentsLs = [
 
 console.log(pluck(studentsLs, "age"));
 console.log(pluck(studentsLs, "name"));
+
+
+/** Event base Event example */
+
+interface BaseEvent {
+    time: number;
+    user: string;
+}
+
+interface EventMap {
+    addToCart: BaseEvent & {quantity: number, totalPrice: number},
+    checkout: BaseEvent
+}
+
+function sendEvent<T extends keyof EventMap>(key: T, data: EventMap[T]) {
+    console.log('Event emitted', data);
+}
+
+sendEvent("addToCart", {time: 23232, user: 'john', quantity: 22, totalPrice: 232.2});
+sendEvent("checkout", {time: 2111, user: 'jack'});
